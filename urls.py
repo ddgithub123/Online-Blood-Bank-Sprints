ccbad4df0885d4d7cdf63c6078126116fef2a3a6
ddgithub123 <125129945+ddgithub123@users.noreply.github.com>
2025-03-04 08:18:53 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from .views import signup_view, login_view, home_view
+
+urlpatterns = [
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('', home_view, name='home'),
+]
